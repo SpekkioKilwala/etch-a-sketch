e76055f07213d6a869aa350ea887e74feef1c068
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-25 11:07:56 +1000

    Put visible text in the draw area with JS

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
 
+const drawArea = document.querySelector('#drawArea');
+// console.log(drawArea);
+
+createGrid(4);
+
+// First: Can put a text into that Draw Area
+function createGrid(sideLength) {
+    const div = document.createElement('div');
+    div.setAttribute('style', 'color: blue; width: 100px; height: 100px;');
+    div.textContent = "A thing!"
+    drawArea.appendChild(div);
+}
