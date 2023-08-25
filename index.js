f1612b77240ae3c1c1cdd8244d32357bf3eff3b5
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-25 11:39:42 +1000

    Add CSS to cells via set rules in .css file

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function createGrid(sideLength) {
   for (let x = 0; x < sideLength; x++) {
     // create a new column
     const column = document.createElement('div');
-    column.setAttribute('style', 'border: 3px;');
     for (let y = 0; y < sideLength; y++) {
         // create, style, and append all the children to that column
         const cell = document.createElement('div');
-        cell.setAttribute('style', 'border: 2px dotted blue; width: 30px; height: 30px');
+        cell.setAttribute('class', 'cell');
         column.appendChild(cell);
     }
     // finally style the column and add it to the div
+    column.setAttribute('class', 'column')
     drawArea.appendChild(column);
   }
 }
