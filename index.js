b04883dedcf373e2bd07d3984cace68163ef3e88
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-25 11:29:56 +1000

    Draw an unstyled grid of divs inside the play area

--- index.js
+++ index.js
@@ -2,13 +2,27 @@
 
 const drawArea = document.querySelector('#drawArea');
 // console.log(drawArea);
+let sideLength = 4;
 
-createGrid(4);
+createGrid(sideLength);
 
-// First: Can put a text into that Draw Area
 function createGrid(sideLength) {
-    const div = document.createElement('div');
-    div.setAttribute('style', 'color: blue; width: 100px; height: 100px;');
-    div.textContent = "A thing!"
-    drawArea.appendChild(div);
+  deleteChildren(drawArea);
+  for (let x = 0; x < sideLength; x++) {
+    // create a new column
+    const column = document.createElement('div');
+    column.setAttribute('style', 'border: 3px;');
+    for (let y = 0; y < sideLength; y++) {
+        // create, style, and append all the children to that column
+        const cell = document.createElement('div');
+        cell.setAttribute('style', 'border: 2px dotted blue; width: 30px; height: 30px');
+        column.appendChild(cell);
+    }
+    // finally style the column and add it to the div
+    drawArea.appendChild(column);
+  }
+}
+
+function deleteChildren(element) {
+  while (element.hasChildNodes()) {element.removeChild(element.firstChild)}
 }
