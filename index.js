f4350c84db4a5f37c3526c1cc0c097c3c5f22a5e
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-25 13:39:04 +1000

    Add colour helper functions, don't work
    I didn't differentiate between working with values
    and working with arrays of values so it's all broken.
    Need to re-examine how I'm working with colours at all.

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 const drawArea = document.querySelector('#drawArea');
 // console.log(drawArea);
 let sideLength = 16;
+const initialColor = [192, 192, 192]; // rgb
+const finalColor = [0, 0, 0]; // still rgb
+const colorIncrements = 10;
 
 createGrid(sideLength);
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
 
 function createGrid(sideLength) {
   deleteChildren(drawArea);
@@ -25,4 +30,32 @@ function createGrid(sideLength) {
 
 function deleteChildren(element) {
   while (element.hasChildNodes()) {element.removeChild(element.firstChild)}
+}
+
+function strRGB(color) {
+  // given a RGB colour like [192, 192, 192]
+  // return the string "rgb(226, 197, 158)" WITHOUT the semicolon.
+  // (because I'll be using this in backtick formatted strings, semicolon is easy to add.)
+  return `rgb(${color.join(", ")})`
+}
+
+function interpolatedStep(initial, min, max, numSteps) {
+  // We have a number scale with limits of min and max,
+  // and an initial value that is hopefully somewhere within that scale.
+  // The distance between min and max has numSteps divisions (or steps).
+  // returns (interpolatedStep + one step) capped to min, max.
+  const step = (max - min)/numSteps;
+  // clamp() needs min < max, so swap values for that
+  if (min > max) {
+    max = [min, min = max][0];
+  }
+  return clamp((initial + step), min, max);
+}
+
+function colorStep(value) {
+  return interpolatedStep(value, initialColor, finalColor, colorIncrements)
+}
+
+function darkenColor(initial) {
+  return initial.map(colorStep)
 }
