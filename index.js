c136bc55a7d5b82a343ca915294535722671e2b3
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-25 16:12:07 +1000

    Shift back to RGB
    Clicking on cells darkens them a little bit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const drawArea = document.querySelector('#drawArea');
 // console.log(drawArea);
 let sideLength = 16;
-// const initialColor = [192, 192, 192]; // rgb
-// const finalColor = [0, 0, 0]; // still rgb
-const initialColor = [0, 0, 75] // hsl
-const finalColor = [0, 0, 0] // hsl
+const initialColor = [192, 192, 192]; // rgb
+const finalColor = [0, 0, 0]; // still rgb
+// const initialColor = [0, 0, 75] // hsl
+// const finalColor = [0, 0, 0] // hsl
 const colorIncrements = 10;
 
 createGrid(sideLength);
@@ -14,6 +14,7 @@ createGrid(sideLength);
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
 
 const toHSLArray = hslStr => hslStr.match(/\d+/g).map(Number);
+const toRGBArray = rgbStr => rgbStr.match(/\d+/g).map(Number);
 
 function createGrid(sideLength) {
   deleteChildren(drawArea);
@@ -24,7 +25,7 @@ function createGrid(sideLength) {
         // create, style, and append all the children to that column
         const cell = document.createElement('div');
         cell.setAttribute('class', 'cell');
-        setBackgroundColor(cell, strHSL(initialColor));
+        setBackgroundColor(cell, strRGB(initialColor));
         cell.addEventListener('click', () => {darken(cell)});
         column.appendChild(cell);
     }
@@ -38,13 +39,21 @@ function deleteChildren(element) {
   while (element.hasChildNodes()) {element.removeChild(element.firstChild)}
 }
 
-function strHSL(color) {
-  // given an HSL colour like [0, 0, 75] (meaning 0, 0%, 75%)
-  // return the string hsl(0, 0%, 75%);
-  // in this case the first value hue is 0-300something, the others are 0-1
+// function strHSL(color) {
+//   // given an HSL colour like [0, 0, 75] (meaning 0, 0%, 75%)
+//   // return the string hsl(0, 0%, 75%);
+//   // in this case the first value hue is 0-300something, the others are 0-1
+//   if (color.length == 3) {
+//     return `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`;
+//   }
+// }
+
+function strRGB(color) {
+// given an RGB colour like [50, 100,150]
+// return the string rgb(50, 100, 150);
   if (color.length == 3) {
-    return `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`;
-  }
+    return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+  }  
 }
 
 function setBackgroundColor(element, color) {
@@ -55,35 +64,62 @@ function setBackgroundColor(element, color) {
 }
 
 function darken(element) {
-  const newColor = shiftHSL(getColor(element), [0, 0, -7.5]);
+  const newColor = shiftRGB(getColor(element), [-10, -10, -10]);
   console.log(newColor);
-  setBackgroundColor(element, newColor);
+  setBackgroundColor(element, strRGB(newColor));
 }
 
 function getColor(element){
   // note that this CAN'T grab what's in the CSS, only
   // what was assigned by the JS.
   // worse yet, this seems to only return it as an RGB.
-  const hsl = element.style.backgroundColor;
-  console.log(hsl);
-  console.log(toHSLArray(hsl));
-  return toHSLArray(hsl);
+  const rgb = element.style.backgroundColor;
+  console.log(rgb);
+  console.log(toRGBArray(rgb));
+  return toRGBArray(rgb);
 }
 
-function shiftHSL(initial, shift) {
-  // given a HSL [A, B, C] and a modification to that [D E F]
-  // return [A+D%360, B+E, C+F]
-  // in the initial version this will usually be used with [0, 0, -7.5]
+// function getColor(element){
+//   // note that this CAN'T grab what's in the CSS, only
+//   // what was assigned by the JS.
+//   // worse yet, this seems to only return it as an RGB.
+//   const hsl = element.style.backgroundColor;
+//   console.log(hsl);
+//   console.log(toHSLArray(hsl));
+//   return toHSLArray(hsl);
+// }
+
+// function shiftHSL(initial, shift) {
+//   // given a HSL [A, B, C] and a modification to that [D E F]
+//   // return [A+D%360, B+E, C+F]
+//   // in the initial version this will usually be used with [0, 0, -7.5]
+//   if ((initial.length == 3) && (shift.length == 3)) {
+//     let h = initial[0] + shift[0];
+//     h = ((h % 360) + 360) % 360; // h has a valid range 0-360
+//     let s = initial[1] + shift[1];
+//     s = clamp(s, 0, 100);
+//     let l = initial[2] + shift[2];
+//     l = clamp(l, 0, 100);
+//     return [h, s, l];
+//   }
+//   console.log(`Can't shift HSL: ${initial} ${shift}`)
+//   return initial
+// }
+
+function shiftRGB(initial, shift) {
+  // given a RGB [A, B, C] and a modification to that [D E F]
+  // return [A+D, B+E, C+F]
+  // in the initial version this will usually be used with [-10, -10, -10]
   if ((initial.length == 3) && (shift.length == 3)) {
-    let h = initial[0] + shift[0];
-    h = ((h % 360) + 360) % 360; // h has a valid range 0-360
-    let s = initial[1] + shift[1];
-    s = clamp(s, 0, 100);
-    let l = initial[2] + shift[2];
-    l = clamp(l, 0, 100);
-    return [h, s, l];
+    let r = initial[0] + shift[0];
+    r = clamp(r, 0, 255)
+    let g = initial[1] + shift[1];
+    g = clamp(g, 0, 255);
+    let b = initial[2] + shift[2];
+    b = clamp(b, 0, 255);
+    return [r, g, b];
   }
-  console.log(`Can't shift HSL: ${initial} ${shift}`)
+  console.log(`Can't shift RGB: ${initial} ${shift}`)
   return initial
 }
 
